af677d3a254a20bc20e76e01a13d6b52af76ec48
Łukasz Piasecki <80245965+LukaszPiasecki13@users.noreply.github.com>
2025-07-27 20:09:48 +0200

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -37,6 +37,7 @@ export default function App() {
   const [throttleStatus, setThrottleStatus] = useState(null);
   const [secondsLeft, setSecondsLeft] = useState(null);
 
+console.log('API_BASE_URL:', process.env.REACT_APP_API_BASE_URL);
 const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
  // const API_BASE_URL = 'https://shortener-z3bg.onrender.com/'
 
