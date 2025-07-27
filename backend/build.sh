f38827e5857e1a901ff3d6f4075e2c732ad8d665
Łukasz Piasecki <80245965+LukaszPiasecki13@users.noreply.github.com>
2025-07-27 17:31:38 +0200

    deployement update

--- backend/build.sh
+++ backend/build.sh
@@ -1,12 +1,14 @@
 /set -o errexit
 
-pip install -r ./backend/requirements.txt
-pip3 install django
+pip install -r ./requirements.txt
 pip install dj-database-url
+pip3 install django
 pip install djangorestframework
 pip install django-cors-headers
+pip install psycopg2-binary
 pip install gunicorn
 pip install uvicorn
+
 
 
 python manage.py collectstatic --no-input
