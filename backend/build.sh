f9301a42318b38b19cd92b25e2ba673f849459a6
Łukasz Piasecki <80245965+LukaszPiasecki13@users.noreply.github.com>
2025-07-27 16:48:31 +0200

    build.sh updatetd

--- backend/build.sh
+++ backend/build.sh
@@ -1,6 +1,10 @@
 /set -o errexit
 
 pip install -r requirements.txt
+pip3 install django
+pip install dj-database-url
+pip install djangorestframework
+pip install django-cors-headers
 pip install gunicorn
 pip install uvicorn
 
