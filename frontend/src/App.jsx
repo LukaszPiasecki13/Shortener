ee48d4c262da8ed53854700b93b899b0dd9c24c0
Łukasz Piasecki <80245965+LukaszPiasecki13@users.noreply.github.com>
2025-07-28 22:09:48 +0200

    small

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -447,7 +447,7 @@ export default function App() {
               >
                 Lukasz Piasecki
               </a>{" "}
-              - Software/Python Developer ✨
+              - Python Software Engineer ✨
             </Text>
           </div>
         </div>
